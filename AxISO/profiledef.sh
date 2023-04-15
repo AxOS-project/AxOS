8f572933b490788d6b70b6927d846894b19db466
Ardox <110931544+LeVraiArdox@users.noreply.github.com>
2023-04-15 21:38:36 +0200

    Updated AxISO

---
+++ AxISO/profiledef.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2034
+
+iso_name="AxOS"
+iso_label="AxOS 2.6.0"
+iso_publisher="Ardox <lvardox@proton.me>"
+iso_application="AxOS Live/Rescue CD"
+iso_version="2.6.0"
+install_dir="arch"
+buildmodes=('iso')
+bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
+           'uefi-ia32.grub.esp' 'uefi-x64.grub.esp'
+           'uefi-ia32.grub.eltorito' 'uefi-x64.grub.eltorito')
+arch="x86_64"
+pacman_conf="pacman.conf"
+airootfs_image_type="squashfs"
+airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
+file_permissions=(
+  ["/etc/shadow"]="0:0:400"
+  ["/root"]="0:0:750"
+  ["/root/.automated_script.sh"]="0:0:755"
+  ["/usr/local/bin/choose-mirror"]="0:0:755"
+  ["/usr/local/bin/Installation_guide"]="0:0:755"
+  ["/usr/local/bin/livecd-sound"]="0:0:755"
+)
